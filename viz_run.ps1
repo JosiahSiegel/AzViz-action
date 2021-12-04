9ac873026dec68f5c8026f806652bf6d5a21f467
Josiah Siegel <josiah.siegel@semanticbits.com>
2021-12-04 02:34:01 -0500

    init

---
+++ viz_run.ps1
@@ -0,0 +1,46 @@
+Param(
+    [Parameter(Mandatory)]
+    [String]$RESOURCE_GROUP,
+    [Parameter(Mandatory)]
+    [String]$OUT_FILE = 'viz.png',
+    [Parameter(Mandatory)]
+    [String]$SUB_NAME = 'Pay-As-You-Go',
+    [Parameter(Mandatory)]
+    [String]$THEME = 'neon',
+    [Parameter(Mandatory)]
+    [String]$DEPTH = '1',
+    [Parameter(Mandatory)]
+    [String]$VERBOSITY = '1',
+    [Parameter(Mandatory)]
+    [String]$FORMAT = 'png',
+    [Parameter(Mandatory)]
+    [String]$DIRECTION = 'top-to-bottom',
+    [String]$EXCLUDE_TYPES = '*excludethisthing1,excludethisthing2*',
+    [Parameter(Mandatory)]
+    [String]$SPLINES = 'spline'
+)
+
+# Create missing directory paths for output
+New-Item -ItemType File -Force -Path ${OUT_FILE}
+
+# Get current Azure context
+$currentAzureContext = Get-AzContext;
+
+# Check If Azure context exists
+if ($currentAzureContext.Tenant.TenantId) {
+
+    # Set Azure subscription to match SUB_NAME
+    Set-AzContext -SubscriptionName ${SUB_NAME};
+};
+
+# Run AzViz and export Azure diagram to location OUT_FILE
+Export-AzViz `
+    -ResourceGroup ${RESOURCE_GROUP}.Split(",") `
+    -Theme ${THEME} `
+    -OutputFormat ${FORMAT} `
+    -CategoryDepth ${DEPTH} `
+    -LabelVerbosity ${VERBOSITY} `
+    -ExcludeTypes ${EXCLUDE_TYPES}.Split(",") `
+    -Splines ${SPLINES} `
+    -Direction ${DIRECTION} `
+    -OutputFilePath ${OUT_FILE};
